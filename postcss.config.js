1eb8376d4059e7bb606b3e47193f0ba6e33ab7ea
TT <tt-tjw@qq.com>
2020-11-28 09:56:03 +0800

    save

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,55 @@
-const path = require('path')
+const path = require("path");
+// Purgecss 编译时裁剪css样式大小
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.jsx",
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  parser: require('postcss-comment'),
+  syntax: "postcss-scss",
+  parser: require("postcss-comment"),
   plugins: [
-    require('postcss-import')({
-      resolve (id, basedir, importOptions) {
-        if (id.startsWith('~@/')) {
-          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(3))
-        } else if (id.startsWith('@/')) {
-          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(2))
-        } else if (id.startsWith('/') && !id.startsWith('//')) {
-          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(1))
+    require("postcss-import")({
+      resolve(id, basedir, importOptions) {
+        if (id.startsWith("~@/")) {
+          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(3));
+        } else if (id.startsWith("@/")) {
+          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(2));
+        } else if (id.startsWith("/") && !id.startsWith("//")) {
+          return path.resolve(process.env.UNI_INPUT_DIR, id.substr(1));
         }
-        return id
-      }
+        return id;
+      },
     }),
-    require('autoprefixer')({
-      remove: process.env.UNI_PLATFORM !== 'h5'
+    require("@dcloudio/vue-cli-plugin-uni/packages/postcss"),
+    require("tailwindcss"),
+    require("autoprefixer")({
+      remove: process.env.UNI_PLATFORM !== "h5",
     }),
-    require('@dcloudio/vue-cli-plugin-uni/packages/postcss')
-  ]
-}
+    // 根据平台差异进行不同的样式处理
+    ...(process.env.UNI_PLATFORM !== "h5"
+      ? [
+          // 使用postcss-class-name 包将小程序不支持的类名转换为支持的类名
+          require("postcss-class-rename")({
+            "\\\\:": "--",
+            "\\\\/": "--",
+          }),
+        ]
+      : [
+          require("autoprefixer")({
+            remove: true,
+          }),
+        ]),
+    // 添加purgecss处理
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    // purgecss,
+  ],
+};
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,41 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+   <view class="content">
+      <image class="logo w-32 h-32" src="/static/logo.png"></image>
+      <view>
+         <view class="title p-4 rounded text-lg flex flex-col">
+            <div class="bg-gray-200 p-4 rounded shadow-lg m-4"> {{title}}</div>
+            <div class="bg-yellow-300  text-white p-4 rounded shadow-lg m-4">
+               {{title}}
+            </div>
+         </view>
+         <button class="px-4 py-2 rounded  border-0 text-white bg-green-500 text-xl shadow-lg">
+            登录</button>
+      </view>
+      <div class="flex space-x-4">
+         <div class="bg-red-400 text-white hover:bg-red-600">
+            1</div>
+         <div class="bg-green-600">2</div>
+         <div class="bg-green-600">3</div>
+         <div>4</div>
+         <div>5</div>
+      </div>
+   </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+   data() {
+      return {
+         title: "Hello",
+      };
+   },
+   onLoad() {},
+   methods: {},
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss">
+.content {
+   @apply flex flex-col items-center justify-center;
+}
 </style>
